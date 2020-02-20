2ca9c75682adcec16cc1514d1da4dc541cef553b
Евгений Альтман <altmanea@gmail.com>
2020-02-20 09:21:28 +0600

    Lesson 2 - kotlinx html

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,50 @@
+import data.studentList
+import kotlinx.html.H1
+import kotlinx.html.dom.append
+import kotlinx.html.js.h1
+import kotlinx.html.js.li
+import kotlinx.html.js.ol
+import kotlinx.html.js.onClickFunction
+import org.w3c.dom.events.Event
+import kotlin.browser.document
+import kotlin.dom.clear
+
+var ascending = true
+
+fun main() {
+    document.getElementById("root")!!
+        .append {
+            h1 {
+                +"Students"
+                onClickFunction = onCLickFunction()
+            }
+            ol {
+                attributes += "id" to "listStudents"
+                studentList.map {
+                    li {
+                        +"${it.firstname} ${it.surname}"
+                    }
+                }
+            }
+        }
+}
+
+private fun H1.onCLickFunction(): (Event) -> Unit {
+    return {
+        val listStudents = document.getElementById("listStudents")!!
+        listStudents.clear()
+        listStudents.append {
+            if (ascending)
+                studentList.sortBy { it.firstname }
+            else
+                studentList.sortByDescending { it.firstname }
+            ascending = !ascending
+            attributes += "id" to "listStudents"
+            studentList.map {
+                li {
+                    +"${it.firstname} ${it.surname}"
+                }
+            }
+        }
+    }
+}
