6a8a553d28733095a05c59f94a3d30a97366af53
Евгений Альтман <altmanea@gmail.com>
2020-03-05 09:51:51 +0600

    lesson 4 - elements and components React

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,78 +1,26 @@
 import data.studentList
-import kotlinx.html.*
-import kotlinx.html.dom.append
-import kotlinx.html.js.*
-import kotlinx.html.js.li
-import kotlinx.html.js.option
 import org.w3c.dom.HTMLElement
 import org.w3c.dom.HTMLSelectElement
 import org.w3c.dom.events.Event
+import react.dom.h1
+import react.dom.li
+import react.dom.ol
+import react.dom.render
 import kotlin.browser.document
 import kotlin.dom.clear
 
-var ascending = true
 
 fun main() {
-    document.getElementById("root")!!
-        .append {
-            h1 {
-                attributes += "id" to "header"
-                +"Students"
-                onClickFunction = onCLickFunction()
-            }
-            ol {
-                attributes += "id" to "listStudents"
-                studentList.map {
-                    li {
-                        +"${it.firstname} ${it.surname}"
-                    }
-                }
-            }
-            select(options = arrayListOf("blue", "green")) {
-                attributes += "id" to "selectColor"
-                onClickFunction = {
-                    val selectColor =
-                        document.getElementById("selectColor")!!
-                                as HTMLSelectElement
-                    val header =
-                        document.getElementById("header")!!
-                    header.setAttribute("style", "color:${selectColor.value}")
-                }
-            }
+    render(document.getElementById("root")!!) {
+        h1 {
+            +"Students"
         }
-}
-
-private fun H1.onCLickFunction(): (Event) -> Unit {
-    return {
-        val listStudents = document.getElementById("listStudents")!!
-        listStudents.clear()
-        listStudents.append {
-            if (ascending)
-                studentList.sortBy { it.firstname }
-            else
-                studentList.sortByDescending { it.firstname }
-            ascending = !ascending
-            studentList.map {
+        ol {
+            studentList.forEach {
                 li {
-                    +"${it.firstname} ${it.surname}"
+                    val tmp = rstudent(it)
                 }
             }
         }
     }
-}
-
-fun TagConsumer<HTMLElement>.select(
-    classes : String? = null,
-    options: List<String>,
-    block : SELECT.() -> Unit = {}
-) : HTMLSelectElement = select(
-    classes
-) {
-    options.forEach {
-        option {
-            attributes += "value" to it
-            +it
-        }
-    }
-    block()
 }
