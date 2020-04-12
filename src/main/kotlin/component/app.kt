1d099c77ab0d73d04a0ad424ee09e8c640779d2d
Евгений Альтман <altmanea@gmail.com>
2020-04-12 11:28:24 +0600

    Lesson 12 - react redux - step 1

--- src/main/kotlin/component/app.kt
+++ src/main/kotlin/component/app.kt
@@ -1,12 +1,10 @@
 package component
 
+import container.*
 import data.*
-import hoc.withDisplayName
-import org.w3c.dom.events.Event
 import react.*
 import react.dom.*
 import react.router.dom.*
-import redux.*
 
 interface AppProps : RProps {
     var store: Store<State, RAction, WrapperAction>
@@ -20,7 +18,8 @@ val AppProps.students
 interface RouteNumberResult : RProps {
     var number: String
 }
-fun RouteResultProps<RouteNumberResult>.num()=
+
+fun RouteResultProps<RouteNumberResult>.num() =
     this.match.params.number.toIntOrNull() ?: -1
 
 fun fApp() =
@@ -39,131 +38,70 @@ fun fApp() =
         switch {
             route("/lessons",
                 exact = true,
-                render = {
-                    anyList(
-                        props.lessons,
-                        "Lessons",
-                        "/lessons",
-                        RBuilder::lesson,
-                        { props.store.dispatch(AddLesson(Lesson("new lesson"))) },
-                        { props.store.dispatch(RemoveLesson(it)) }
-                    )
-                }
+                render = { lessonListContainer { } }
             )
             route("/students",
                 exact = true,
-                render = {
-                    anyList(
-                        props.students,
-                        "Students",
-                        "/students",
-                        RBuilder::student,
-                        { props.store.dispatch(AddStudent(Student("new", "student")))},
-                        { props.store.dispatch(RemoveStudent(it))}
-                    )
-                }
+                render = { studentListContainer {} }
             )
             route(
                 "/lessons/:number",
                 exact = true,
-                render = renderLesson(props)
+                render = renderObject(
+                    { props.lessons[it] },
+                    { index, lesson ->
+                        lessonFullContainer {
+                            attrs.obj = index to lesson
+                        }
+                    }
+                )
             )
             route(
                 "/students/:number",
                 exact = true,
-                render = renderStudent(props)
+                render = renderObject(
+                    { props.students[it] },
+                    { index, student ->
+                        studentFullContainer {
+                            attrs.obj = index to student
+                        }
+                    }
+                )
             )
             route(
                 "/lessons/:number/edit",
-                render = renderLessonEdit(props)
+                render = renderObject(
+                    { props.lessons[it] },
+                    { index, lesson ->
+                        lessonEditContainer {
+                            attrs.obj = index to lesson
+                        }
+                    }
+                )
             )
             route(
                 "/students/:number/edit",
-                render = renderStudentEdit(props)
+                render = renderObject(
+                    { props.students[it] },
+                    { index, student ->
+                        studentEditContainer {
+                            attrs.obj = index to student
+                        }
+                    }
+                )
             )
         }
     }
 
-fun AppProps.onClickStudent(num: Int): (Int) -> (Event) -> Unit =
-    { index ->
-        {
-            store.dispatch(ChangePresent(index, num))
-        }
-    }
-
-fun AppProps.onClickLesson(num: Int): (Int) -> (Event) -> Unit =
-    { index ->
-        {
-            store.dispatch(ChangePresent(num, index))
-        }
-    }
-
-fun RBuilder.renderLesson(props: AppProps) =
-    { route_props: RouteResultProps<RouteNumberResult> ->
-        val num = route_props.num()
-        val lesson = props.lessons[num]
-        if (lesson != null)
-            anyFull(
-                RBuilder::student,
-                num to lesson,
-                props.store.state.students,
-                props.store.state.presents[num],
-                props.onClickLesson(num)
-            )
-        else
-            p { +"No such lesson" }
-    }
-
-fun RBuilder.renderStudent(props: AppProps) =
-    { route_props: RouteResultProps<RouteNumberResult> ->
-        val num = route_props.num()
-        val student = props.store.state.students[num]
-        if (student != null)
-            anyFull(
-                RBuilder::lesson,
-                num to student,
-                props.store.state.lessons,
-                props.store.state.presentsStudent(num),
-                props.onClickStudent(num)
-            )
-        else
-            p { +"No such student" }
-    }
-
-
-fun RBuilder.renderLessonEdit(props: AppProps) =
-    { route_props: RouteResultProps<RouteNumberResult> ->
-        val num = route_props.num()
-        val lesson = props.lessons[num]
-        if (lesson != null) {
-            lessonEdit(
-                num to lesson
-            ) { props.store.dispatch(ChangeLesson(num, it))}
-        }
-        else
-            p { +"No such lesson" }
-    }
-
-fun RBuilder.renderStudentEdit(props: AppProps) =
-    { route_props: RouteResultProps<RouteNumberResult> ->
-        val num = route_props.num()
-        val student = props.students[num]
-        if (student != null) {
-            studentEdit(
-                num to student
-            ) { props.store.dispatch(ChangeStudent(num, it))}
-        }
-        else
-            p { +"No such lesson" }
-    }
-
-fun RBuilder.app(
-    store: Store<State, RAction, WrapperAction>
+fun <O> RBuilder.renderObject(
+    selector: (Int) -> O?,
+    rElement: (Int, O) -> ReactElement
 ) =
-    child(
-        withDisplayName("App", fApp())
-    ) {
-        attrs.store = store
+    { route_props: RouteResultProps<RouteNumberResult> ->
+        val num = route_props.num()
+        val obj = selector(num)
+        if (obj != null)
+            rElement(num, obj)
+        else
+            p { +"Object not found" }
     }
-
-
